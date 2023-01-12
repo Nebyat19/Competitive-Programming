43a9b04c7c97c8f47ece2290d3e5632de63540ad
Nebyat Bekele(hetu) <64023691+Nebyat19@users.noreply.github.com>
2023-01-12 11:57:59 +0300

    Create Boats-to-Save-People.cpp

---
+++ week2/Boats-to-Save-People.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    int numRescueBoats(vector<int>& people, int limit) {
+       
+      sort(people.begin(),people.end());
+      int count=0;int wgt=0;
+       int i=0; int j=people.size()-1;
+        while(i<j){
+      wgt=people[i]+people[j];
+       if(wgt<=limit) {
+           
+            count++;
+            i++;
+            j--;
+            }else{
+                count++;
+                j--;
+            }
+           
+        }if(i==j)count++;
+        return count;
+    }
+};
